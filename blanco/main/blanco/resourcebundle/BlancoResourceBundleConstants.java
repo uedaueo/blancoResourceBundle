79bcd2653ce14f9550992ce03bbeb6626cca041f
junji.amano <junji.amano.sigrest@gmail.com>
2016-01-28 19:38:35 +0900

    jxl2poi

--- blanco/main/blanco/resourcebundle/BlancoResourceBundleConstants.java
+++ blanco/main/blanco/resourcebundle/BlancoResourceBundleConstants.java
@@ -29,7 +29,7 @@ public class BlancoResourceBundleConstants {
      * 項目番号:3<br>
      * バージョン番号。
      */
-    public static final String VERSION = "1.3.10";
+    public static final String VERSION = "2.0.1";
 
     /**
      * 項目番号:4<br>
